ff3e2dd45b3b2723e320e24862948eabe5560ea2
Piotr <piotr_as@yahoo.com>
2018-10-05 12:40:05 +0200

    the end of exe #1

--- root/scripts/01.js
+++ root/scripts/01.js
@@ -1,20 +1,85 @@
 // --------------------- EXE #1 --------------------- //
-            // Array Cardio practice:
+// Array Cardio practice:
 
 // My experiments with filter method:
 
-const people = [
-  {name: 'Jan', last: 'As', age:29, city: 'Poznan', post_code:'01-232' },
-  {name: 'Anna', last: 'Sowa', age:32, city: 'Krakow', post_code:'02-432'  },
-  {name: 'Piotr', last: 'Augustyn', age:37, city: 'Lodz', post_code:'90-249'  },
-  {name: 'Pawel', last: 'Pawlowski', age:43, city: 'Warszawa', post_code:'02-847'  },
-  {name: 'Janina', last: 'Pawlak', age:78, city: 'Lodz', post_code:'90-234'  },
-  {name: 'Adrian', last: 'Wetuj', age:42, city: 'Warsaw', post_code:'01-100'  },
-  {name: 'Antoni', last: 'Smolenski', age:70, city: 'Warsaw', post_code:'01-123'  },
-  {name: 'Kazik', last: 'Staszewski', age:59, city: 'Wroclaw', post_code:'89-345'  },
-  {name: 'Monika', last: 'Bikini', age:20, city: 'Poznan', post_code: '01-345' },
-  {name: 'Karol', last: 'Wisniewski', age:34, city: 'Katowice', post_code:'10-897'  },
-  {name: 'Roman', last: 'Traktor', age:67, city: 'Zadupie', post_code:'00-000'  }
+const people = [{
+        name: 'Jan',
+        last: 'As',
+        age: 29,
+        city: 'Poznan',
+        post_code: '01-232'
+    },
+    {
+        name: 'Anna',
+        last: 'Sowa',
+        age: 32,
+        city: 'Krakow',
+        post_code: '02-432'
+    },
+    {
+        name: 'Piotr',
+        last: 'Augustyn',
+        age: 37,
+        city: 'Lodz',
+        post_code: '90-249'
+    },
+    {
+        name: 'Pawel',
+        last: 'Pawlowski',
+        age: 43,
+        city: 'Warszawa',
+        post_code: '02-847'
+    },
+    {
+        name: 'Janina',
+        last: 'Pawlak',
+        age: 78,
+        city: 'Lodz',
+        post_code: '90-234'
+    },
+    {
+        name: 'Adrian',
+        last: 'Wetuj',
+        age: 42,
+        city: 'Warsaw',
+        post_code: '01-100'
+    },
+    {
+        name: 'Antoni',
+        last: 'Smolenski',
+        age: 70,
+        city: 'Warsaw',
+        post_code: '01-123'
+    },
+    {
+        name: 'Kazik',
+        last: 'Staszewski',
+        age: 59,
+        city: 'Wroclaw',
+        post_code: '89-345'
+    },
+    {
+        name: 'Monika',
+        last: 'Bikini',
+        age: 20,
+        city: 'Poznan',
+        post_code: '01-345'
+    },
+    {
+        name: 'Karol',
+        last: 'Wisniewski',
+        age: 34,
+        city: 'Katowice',
+        post_code: '10-897'
+    },
+    {
+        name: 'Roman',
+        last: 'Traktor',
+        age: 67,
+        city: 'Zadupie',
+        post_code: '00-000'
+    }
 ];
 
 
@@ -54,7 +119,18 @@ const people = [
 window.addEventListener('keydown', function (e) {
     e.preventDefault();
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-})
+    key.classList.add('playing');
+
+});
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
